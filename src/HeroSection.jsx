071b8e6c4f44d5ce60709b1b2fec98009bd4cb09
Yinka <asyinka@gmail.com>
2023-04-26 12:18:34 +0100

    chore: style hero button and hero image

--- src/HeroSection.jsx
+++ src/HeroSection.jsx
@@ -1,23 +1,28 @@
 const HeroSection = () => {
   return (
-    <div className="hero">
-      <header className="hero__headings">
-        <h1 className="header-text header-text1">Collect.</h1>
-        <h1 className="header-text header-text2">Manage.</h1>
-        <h1 className="header-text header-text3">Share.</h1>
-      </header>
-      <article className="hero__article">
-        Reviews from satisfied clients/customers can now be collected easily
-        with ReviewGush using video, images, text, ratings, or even a
-        combination of all four formats.
-      </article>
-      <button className="hero__button">Start Here</button>
-      <picture className="hero__picture">
-        <img
-          src="./assets/images/developer.svg"
-          alt="image of a developer working on his system"
-        />
-      </picture>
+    <div className="hero-container">
+      <div className="hero">
+        <header className="hero__heading">
+          <h1 className="header-text--primary">Collect.</h1>
+          <h1 className="header-text--secondary">Manage.</h1>
+          <h1 className="header-text--primary">Share.</h1>
+        </header>
+        <article className="hero__article">
+          Reviews from satisfied clients/customers can now be collected easily
+          with ReviewGush using video, images, text, ratings, or even a
+          combination of all four formats.
+        </article>
+        <button className="hero__button button-primary button-primary--text">
+          Start Here
+        </button>
+        <picture className="hero__picture--container">
+          <img
+            className="hero__picture"
+            src="./src/assets/images/developer.svg"
+            alt="image of a developer working on his system"
+          />
+        </picture>
+      </div>
     </div>
   );
 };
