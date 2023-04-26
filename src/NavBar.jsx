3c823a3d36dd9cf0ece77d981dfc756b8942e027
Yinka <asyinka@gmail.com>
2023-04-26 08:09:27 +0100

    chore: style navBar

--- src/NavBar.jsx
+++ src/NavBar.jsx
@@ -2,7 +2,7 @@ const NavBar = () => {
   return (
     <div className="navBar">
       <h2 className="navBar-Heading projectName">REVIEWGUSH</h2>
-      <button className="navBar-Button">Get Started</button>
+      <button className="navBar-Button button-primary">Get Started</button>
     </div>
   );
 };
