5a42952e285d3edecd3fc0fbfde4e31021b256c6
Yinka <asyinka@gmail.com>
2023-04-26 08:33:21 +0100

    chore: add more style to navBar button

--- src/NavBar.jsx
+++ src/NavBar.jsx
@@ -2,7 +2,9 @@ const NavBar = () => {
   return (
     <div className="navBar">
       <h2 className="navBar-Heading projectName">REVIEWGUSH</h2>
-      <button className="navBar-Button button-primary">Get Started</button>
+      <button className="navBar-Button button-primary button-primary--text">
+        Get Started
+      </button>
     </div>
   );
 };
