da92ee01fb3fef2d81b1c5b4cb61a0f64718e3e2
Yinka <asyinka@gmail.com>
2023-04-26 09:43:55 +0100

    chore: import images to images folder

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import "./App.css";
+import HeroSection from "./HeroSection";
 import NavBar from "./NavBar";
 const App = () => {
   return (
     <div>
       <NavBar />
+      <HeroSection />
     </div>
   );
 };
