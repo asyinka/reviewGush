6608a6e71b90f281afc92e5236605b3c1ad09267
Yinka <asyinka@gmail.com>
2023-04-27 12:53:43 +0100

    chore: complete basic componets and styles

--- src/assets/goToFormFctn.jsx
+++ src/assets/goToFormFctn.jsx
@@ -1,5 +1,20 @@
+import { useRef } from "react";
+import WaitlistSection from "../Waitlist";
+
 export default function scrollToWaitList() {
-  const waitlist = document.getElementsByClassName("waitlist-card");
-  const waitlistPosition = waitlist.offsetTop;
-  window.scrollTo({ top: waitlistPosition, behavior: "smooth" });
+  const waitlistRef = useRef(null);
+  const handleClick = () => {
+    const waitlistPosition = waitlistRef.current.offsetTop;
+    window.scrollTo({ top: waitlistPosition, behavior: "smooth" });
+  };
+  return (
+    <>
+      <WaitlistSection ref={waitlistRef} />
+      <ButtonComp
+        HandleClick={handleClick}
+        className="navBar-Button button-primary button-primary--text"
+        btnTxt="Get Started"
+      />
+    </>
+  );
 }
